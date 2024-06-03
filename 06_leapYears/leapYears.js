c9abd792420cfd9072c652ef03049fa30320242d
Suchitha Ramachandrapurapu <66826980+suchi-291@users.noreply.github.com>
2024-06-03 20:17:46 +0530

    06_leapYears solution added

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let centuryYear;
+    
+    if(year%100 === 0){
+        centuryYear = year;
+        return (centuryYear%400 === 0 || false);
+    }else
+    return (year%4 === 0 || false);
 };
 
 // Do not edit below this line
