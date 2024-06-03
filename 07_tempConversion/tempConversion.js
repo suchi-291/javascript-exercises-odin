a519c27857296a8597d010a5927ede42f6b32447
Suchitha Ramachandrapurapu <66826980+suchi-291@users.noreply.github.com>
2024-06-03 21:03:02 +0530

    07_tempConversion solution added

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  return Number(((temp - 32)/1.8).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+
+  return Number(((temp*1.8) + 32).toFixed(1));
 };
 
 // Do not edit below this line
