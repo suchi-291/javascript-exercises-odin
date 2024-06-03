bb75c20048308ff75a6ec0488e19bcc73bac78b2
Suchitha Ramachandrapurapu <66826980+suchi-291@users.noreply.github.com>
2024-06-03 20:00:00 +0530

    05-sumAll soultion added

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let sum = 0;
+    let temp;
+    if(min > max){
+        temp = min;
+        min = max;
+        max = temp;
+    }
 
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0) return "ERROR";
+
+    for(let i = min ; i <= max ; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
+//console.log(sumAll(1, 4000));
 // Do not edit below this line
 module.exports = sumAll;
