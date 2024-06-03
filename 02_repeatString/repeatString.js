33b3e14c67119e743b886a4cc1969ee678bbdfaa
Suchitha Ramachandrapurapu <66826980+suchi-291@users.noreply.github.com>
2024-06-03 17:35:14 +0530

    02.repeatString solution added

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+let outputVal = '';
+if(num > 0){
+for( let i = 0; i < num ; i++){
+    outputVal += str;
+}
 
+}else if(num === 0){
+    outputVal = '';
+}else{
+    outputVal = 'ERROR';
+}
+
+return outputVal;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
